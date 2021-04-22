f4deeea011628a7181cecbc1b43c47d19d5c0c19
freddy31_ag@hotmail.com <freddy31_ag@hotmail.com>
2021-04-21 22:45:38 -0500

    section-1

--- Missions_to_Mars/scrape.py
+++ Missions_to_Mars/scrape.py
@@ -5,11 +5,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def scrape_info():
+    #SET UP THE BROWSER
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    return_data = {}
-    driver.close()
-    return return_data
+    url="https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    driver.get(url)
+    html= driver.page_source
+    soup=bs(html,"lxml")
+    wdiv=soup.find("content_title")
     
+    import ipdb; ipdb.set_trace()
+
+
+
+    time.sleep(10)
+    #return_data = {}
+   #driver.close()
+    #return return_data
 
 if __name__ == "__main__":
     print(scrape_info() )
