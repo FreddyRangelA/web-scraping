735331f7556a31e872adba9c6121987200887d43
freddy31_ag@hotmail.com <freddy31_ag@hotmail.com>
2021-04-23 01:48:42 -0500

    py

--- Missions_to_Mars/scrape.py
+++ Missions_to_Mars/scrape.py
@@ -9,17 +9,30 @@ def scrape_info():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     url="https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     driver.get(url)
+
     html= driver.page_source
     soup=bs(html,"lxml")
-    wdiv=soup.find("content_title")
+
+    wdiv=soup.find('ul', "item_list")
     
-    import ipdb; ipdb.set_trace()
+    news_title = wdiv.find_all('div', "content_title")[0].text
+    print(news_title)
 
+    news_p = wdiv.find_all('div', "article_teaser_body")[0].text
+    print(news_p)
 
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    url_jpl="https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+    driver.get(url_jpl)
+
+    html= driver.page_source
+    soup=bs(html,"lxml")
+
+    
 
     time.sleep(10)
     #return_data = {}
-   #driver.close()
+    driver.close()
     #return return_data
 
 if __name__ == "__main__":
